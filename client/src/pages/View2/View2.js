79f890d7a63dbcbb8a698a85e31a809147a93051
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-10-16 10:47:29 +0600

    view-2 set-up done

--- client/src/pages/View2/View2.js
+++ client/src/pages/View2/View2.js
@@ -1,16 +1,73 @@
 // external components
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import BookingPopUp from "../../components/BookingPopUp/BookingPopUp";
 
 // internal components
 import "./View2.css";
 
-const View2 = () => {
+const View1 = () => {
+	// for updating booking
+	const [isUpdate, setIsUpdate] = useState("");
+
+	// for getting selected seat
+	const [getId, setId] = useState("");
+
+	// for displaying booked value
+	const [getBooked, setBooked] = useState("");
+
+	// for getting all documents
+	const [getDocs, setDocs] = useState("");
+
+	// fetching data from database start
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await fetch("/user/allDocs?lab=ridhima-girls-lab-2");
+
+				const result = await response.json();
+
+				if (response.status === 200) {
+					setDocs(result ? result : []);
+				} else if (response.status === 400) {
+					toast(result.error, {
+						position: "top-right",
+						theme: "dark",
+						autoClose: 3000
+					});
+				}
+			} catch (error) {
+				toast.error(error.message, {
+					position: "top-right",
+					theme: "colored",
+					autoClose: 3000
+				});
+			}
+		})();
+	}, [isUpdate]);
+	// fetching data from database end
+
 	return (
-		<div className="container-fluid p-0 view-2-main-container">
-			<div className="row m-0 view-2-container">
-				<div className="col-xl-10 col-lg-11 col-11 p-0 view-2-wrapper"></div>
-			</div>
-		</div>
+		<>
+			{getDocs.length > 0 && (
+				<div className="container-fluid p-0 view-2-main-container">
+					<div className="row m-0 view-2-container">
+						<div className="col-xl-10 col-lg-11 col-11 p-0 view-2-wrapper"></div>
+					</div>
+					{(getId || getBooked) && (
+						<BookingPopUp
+							getId={getId}
+							setId={setId}
+							frow_where={"ridhima-girls-lab-1"}
+							setIsUpdate={setIsUpdate}
+							getBooked={getBooked}
+							setBooked={setBooked}
+						/>
+					)}
+				</div>
+			)}
+		</>
 	);
 };
 
-export default View2;
+export default View1;
